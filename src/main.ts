67e31d6b32aed90e522ed2b8957c8915dccc9cb4
Jayala101 <jimmysanti1@hotmail.com>
2025-07-13 20:09:49 -0500

    change port to 3030

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     credentials: true,
   });
   
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3030;
   await app.listen(port);
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
