8f283ead58b9436ac2dc8e0ae72fad4784b1867e
Jayala101 <jimmysanti1@hotmail.com>
2025-07-15 01:58:41 -0500

    Deploy health endpoint to production - version 2025.07.15.01.51

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHealth(): { service: string; version: string, status: boolean } {
     return {
       service: 'nestjs-blog-backend-api',
-      version: '2025.07.12.17.01',
+      version: '2025.07.15.01.51',
       status: true,
     };
   }
